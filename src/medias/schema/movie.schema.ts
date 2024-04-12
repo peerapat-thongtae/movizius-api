5776326b142af0ef1c1b7727fd5923e846724d5c
peerapat-thongtae <peera.thongtae@gmail.com>
2024-04-12 14:26:35 +0700

    update plugin

--- src/medias/schema/movie.schema.ts
+++ src/medias/schema/movie.schema.ts
@@ -1,6 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
-import * as paginate from 'mongoose-paginate-v2';
+import paginate from 'mongoose-paginate-v2';
 
 export type MovieDocument = Movie & Document;
 

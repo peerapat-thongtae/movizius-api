4018914f714b8c9c0e9621ece3edd62424fa5d9c
peerapat-thongtae <peera.thongtae@gmail.com>
2024-05-30 01:25:52 +0700

    log hello

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  private readonly logger = new Logger(AppService.name);
 
   @Get()
   getHello(): string {
+    this.logger.warn('hello');
+    console.log('hello');
     return this.appService.getHello();
   }
 }
